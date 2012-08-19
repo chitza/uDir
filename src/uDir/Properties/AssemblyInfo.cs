19a6e9b5189c0776ae33a526ddc352f16cef169f
Cristi Paraschiv <chitza@gmail.com>
2012-08-19 18:27:40 +0300

    proper IPC using CopyDataStruct

--- src/uDir/Properties/AssemblyInfo.cs
+++ src/uDir/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Chitza")]
 [assembly: AssemblyProduct("uDir")]
-[assembly: AssemblyCopyright("Copyright © Chitza 2010-2011")]
+[assembly: AssemblyCopyright("Copyright © Chitza 2010-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
 
 [assembly: InternalsVisibleTo("uDir.Tests")]
--- src/uDir/WinAPI.cs
+++ src/uDir/WinAPI.cs
@@ -6,56 +6,68 @@ using System.Runtime.InteropServices;
 
 namespace uDir
 {
-    public class WinAPI
-    {
-        public static readonly uint WM_COPYDATA = 0x004a;
+	public class WinAPI
+	{
+		public static readonly uint WM_COPYDATA = 0x004a;
 
-        [DllImport("user32.dll")]
-        public static extern UInt32 RegisterWindowMessage(string lpString);
+		[DllImport("user32.dll")]
+		public static extern UInt32 RegisterWindowMessage(string lpString);
 
-        [DllImport("user32.dll")]
-        public static extern IntPtr SendMessageTimeout(IntPtr windowHandle, uint Msg, 
-                                                        IntPtr wParam, ref CopyDataStruct lParam, 
-                                                        SendMessageTimeoutFlags flags, uint timeout, out IntPtr result);
+		[DllImport("user32.dll")]
+		public static extern IntPtr SendMessageTimeout(IntPtr windowHandle, uint Msg,
+														IntPtr wParam, IntPtr lParam, 
+														SendMessageTimeoutFlags flags, uint timeout, out IntPtr result);
 
-        [Flags]
-        public enum SendMessageTimeoutFlags : uint
-        {
-            SMTO_NORMAL = 0x0000,
-            SMTO_BLOCK = 0x0001,
-            SMTO_ABORTIFHUNG = 0x0002,
-            SMTO_NOTIMEOUTIFNOTHUNG = 0x0008
-        }
+		[Flags]
+		public enum SendMessageTimeoutFlags : uint
+		{
+			SMTO_NORMAL = 0x0000,
+			SMTO_BLOCK = 0x0001,
+			SMTO_ABORTIFHUNG = 0x0002,
+			SMTO_NOTIMEOUTIFNOTHUNG = 0x0008
+		}
 
-        [StructLayout(LayoutKind.Sequential)]
-        public struct CopyDataStruct
-        {
-            public string ID;
-            public int Length;
-            public string Data;
-        }
+		[StructLayout(LayoutKind.Sequential)]
+		public struct CopyDataStruct
+		{
+			public IntPtr dwData;
+			public int cbData;
+			public IntPtr lpData;
+		}
 
-        #region SendStringToWindow
-        /// <summary>
-        /// Sends a string to a window.
-        /// </summary>
-        /// <param name="destinationHandle">Destination handle</param>
-        /// <param name="message">The string to be sent.</param>
-        /// <param name="senderHandle">Handle of the sender.</param>
-        /// <returns>Returns true if sending the message succeeded.</returns>
-        public static bool SendStringToWindow(IntPtr destinationHandle, string message, IntPtr senderHandle, uint timeout = 100)
-        {
-            WinAPI.CopyDataStruct data = new WinAPI.CopyDataStruct();
+		public static IntPtr CreateCopyDataObj(string message)
+		{
+			IntPtr lpData = Marshal.StringToHGlobalUni(message);
 
-            data.ID = "1";
-            data.Data = message;
-            data.Length = data.Data.Length + 1;
+			CopyDataStruct data = new CopyDataStruct();
+			data.dwData = IntPtr.Zero;
+			data.cbData = message.Length * 2;
+			data.lpData = lpData;
 
-            IntPtr result;
-            IntPtr retVal = WinAPI.SendMessageTimeout(destinationHandle, WM_COPYDATA, senderHandle, ref data, SendMessageTimeoutFlags.SMTO_NORMAL, timeout, out result);
+			IntPtr lpStruct = Marshal.AllocHGlobal(Marshal.SizeOf(data));
 
-            return (retVal != IntPtr.Zero);
-        } 
-        #endregion
-    }
+			Marshal.StructureToPtr(data, lpStruct, false);
+
+			return lpStruct;
+		}
+
+		#region SendStringToWindow
+		/// <summary>
+		/// Sends a string to a window.
+		/// </summary>
+		/// <param name="destinationHandle">Destination handle</param>
+		/// <param name="message">The string to be sent.</param>
+		/// <param name="senderHandle">Handle of the sender.</param>
+		/// <returns>Returns true if sending the message succeeded.</returns>
+		public static bool SendStringToWindow(IntPtr destinationHandle, string message, IntPtr senderHandle, uint timeout = 100)
+		{
+			IntPtr data = WinAPI.CreateCopyDataObj(message);
+
+			IntPtr result;
+			IntPtr retVal = WinAPI.SendMessageTimeout(destinationHandle, WM_COPYDATA, senderHandle, data, SendMessageTimeoutFlags.SMTO_NORMAL, timeout, out result);
+
+			return (retVal != IntPtr.Zero);
+		} 
+		#endregion
+	}
 }
